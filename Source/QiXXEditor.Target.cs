a16e50af01acef042b460d2ca5664da82155c17d
Christophe Desplanches <kcdesplanches@gmail.com>
2026-08-08 20:26:00 -0400

    Migrate to UE 5.8

--- Source/QiXXEditor.Target.cs
+++ Source/QiXXEditor.Target.cs
@@ -8,8 +8,9 @@ public class QiXXEditorTarget : TargetRules
 	public QiXXEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
+		bOverrideBuildEnvironment = true;
 		DefaultBuildSettings = BuildSettingsVersion.V6;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
 		ExtraModuleNames.Add("QiXX");
 	}
 }
